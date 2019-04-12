5a607f070f86d6fc14504431072b0201227381bf
Jun Hyek Jang <41278739+jj2883@users.noreply.github.com>
2019-04-12 07:53:15 -0400

    Add files via upload

--- server.1.py
+++ server.1.py
@@ -201,7 +201,7 @@ class List_Search(MethodView):
 
             	cursor = g.conn.execute(query, (search_ph,))
 
-            elif:
+            else:
 
             	query = "SELECT home_team_name, points_home_team, away_team_name, points_away_team FROM game g, (select * from play_) pl where pl.game_id = g.game_id and (g.away_team_name={} or g.home_team_name={});".format(search,search)
 
